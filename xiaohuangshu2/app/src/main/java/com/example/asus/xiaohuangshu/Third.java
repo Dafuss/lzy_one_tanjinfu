87056727fa79f76c08ad362d5acf1db39b975c96
unknown <1418261960@qq.com>
2019-05-24 22:09:20 +0800

    小黄书app

---
+++ xiaohuangshu2/app/src/main/java/com/example/asus/xiaohuangshu/Third.java
@@ -0,0 +1,14 @@
+package com.example.asus.xiaohuangshu;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Third extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third);
+    }
+}
